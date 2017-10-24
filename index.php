6d26b0f88c528f6fa676bd09f5f7c4f672505464
root <christian.hernandez@yahoo.com>
2017-10-24 16:04:05 -0700

    No time to commit.. My people need me!

--- index.php
+++ index.php
@@ -10,8 +10,23 @@ if (isset($_SESSION['user_logged_in'])) {
 <html>
 <head>
 	<title>Welcome to Orpheus!</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-	Hello!
+	<div id="nowPlayingBarContainer">
+		<div id="nowPlayingBar">
+			
+			<div id="nowPlayingLeft">
+			</div>
+
+			<div id="nowPlayingCenter">
+			</div>
+
+			<div id="nowPlayingRight">
+			</div>
+
+		</div> <!-- now playing bar-->
+
+	</div> <!-- now playing bar container -->
 </body>
 </html>
