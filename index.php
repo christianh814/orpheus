843c27899333af2e6de1081efdc6398ef2350e6c
root <christian.hernandez@yahoo.com>
2017-10-24 17:30:32 -0700

    fixed mistaken bug

--- index.php
+++ index.php
@@ -17,13 +17,54 @@ if (isset($_SESSION['user_logged_in'])) {
 		<div id="nowPlayingBar">
 			
 			<div id="nowPlayingLeft">
-			</div>
+			</div> <!-- now playing left -->
 
 			<div id="nowPlayingCenter">
-			</div>
+				<div class="content playerControls">
+					<div class="buttons">
+						<button class="controlButton shuffle" title="Shuffle Button" alt="shuffle">
+							<img src="assets/images/icons/shuffle.png"></img>
+						</button>
+
+						<button class="controlButton previous" title="Previous Button" alt="previous">
+							<img src="assets/images/icons/previous.png"></img>
+						</button>
+
+						<button class="controlButton play" title="Play Button" alt="play">
+							<img src="assets/images/icons/play.png"></img>
+						</button>
+
+						<button class="controlButton pause" title="Pause Button" alt="pause" style="display:none;">
+							<img src="assets/images/icons/pause.png"></img>
+						</button>
+
+						<button class="controlButton next" title="Next Button" alt="next">
+							<img src="assets/images/icons/next.png"></img>
+						</button>
+
+						<button class="controlButton repeat" title="Repeat Button" alt="repeat">
+							<img src="assets/images/icons/repeat.png"></img>
+						</button>
+
+					</div><!-- buttons -->
+					
+					<div class="playbackBar">
+						<span class="progressTime current">0.00</span>
+
+						<div class="progressBar">
+							<div class="progressBarBg">
+								<div class="progress"></div>
+							</div> <!-- PB bg -->
+						</div><!-- progress bar-->
+
+						<span class="progressTime remaining">0.00</span>
+					</div><!-- playback bar -->
+
+				</div>
+			</div> <!-- now playing center -->
 
 			<div id="nowPlayingRight">
-			</div>
+			</div> <!-- now playing right -->
 
 		</div> <!-- now playing bar-->
 
