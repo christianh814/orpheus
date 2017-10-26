0bede16e37c0446cf0b5f68b6561efd6168906e8
root <christian.hernandez@yahoo.com>
2017-10-25 17:58:52 -0700

    somebody keeps erasing my changes.

--- index.php
+++ index.php
@@ -13,6 +13,42 @@ if (isset($_SESSION['user_logged_in'])) {
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
+<div id="mainContainer">
+
+	<div id="topContainer">
+
+		<div id="navBarContainer">
+			<nav class="navBar">
+				<a href="index.php" class="logo" style="text-decoration: none;">
+					<img src="assets/images/icons/orpheus-logo.png"></img>
+					&nbsp;&nbsp;&nbsp;<i class="navItemLink">orpheus</i>
+				</a>
+
+				<div class="group">
+					<div class="navItem">
+						<a href="search.php" class="navItemLink">Search
+							<img src="assets/images/icons/search.png" class="icon" alt="Search"></img>
+						</a>
+					</div>
+				</div>
+
+				<div class="group">
+					<div class="navItem">
+						<a href="browse.php" class="navItemLink">Browse</a>
+					</div>
+					<div class="navItem">
+						<a href="yourmusic.php" class="navItemLink">Your Music</a>
+					</div>
+					<div class="navItem">
+						<a href="profile.php" class="navItemLink"><?php echo $user_logged_in ?></a>
+					</div>
+				</div>
+
+			</nav>
+		</div> <!-- navBar container -->
+
+	</div> <!-- topContainer contains both left hand nav and right "main" page -->
+
 	<div id="nowPlayingBarContainer">
 		<div id="nowPlayingBar">
 			
@@ -96,5 +132,6 @@ if (isset($_SESSION['user_logged_in'])) {
 		</div> <!-- now playing bar-->
 
 	</div> <!-- now playing bar container -->
+</div> <!--  mainContainer -->
 </body>
 </html>
